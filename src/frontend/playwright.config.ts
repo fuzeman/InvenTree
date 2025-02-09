43271687dcad7fa786370d0a2c111f39b3568cbf
Dean Gardiner <me@dgardiner.net>
2025-02-10 00:48:02 +1300

    [CI] Updated playwright to use sharding instead of multiple workers
    After testing is completed, reports are automatically merged and published
    to GitHub and CodeCov inside the new `platform_ui_reports` job.

--- src/frontend/playwright.config.ts
+++ src/frontend/playwright.config.ts
@@ -2,11 +2,10 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
-  fullyParallel: true,
   timeout: 90000,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 3 : undefined,
+  workers: process.env.CI ? 1 : undefined,
   reporter: process.env.CI ? [['html', { open: 'never' }], ['github']] : 'list',
 
   /* Configure projects for major browsers */
